38a1a5a4d72dfccd8ed417623ba279c5286591ec
Sibi Siddharth S <sibisiddharth008@gmail.com>
2024-12-15 18:51:38 +0530

    mongodb setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
+app.get('/products', (req, res) => {});
 app.listen(5000, () => {
+    connectDB();
     console.log('Server started on port 5000');
 });
