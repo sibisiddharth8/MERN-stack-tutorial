35a6fda8cd459e62fcb6590c84ab696e8300f81d
Sibi Siddharth S <sibisiddharth008@gmail.com>
2024-12-20 17:28:36 +0530

    deployment setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
+import path from 'path';
 
 import productRoutes from './routes/products.route.js'
 
@@ -8,10 +9,21 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+
+// Serve static assets in production
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
 
 // Start the server
 app.listen(PORT, () => {
